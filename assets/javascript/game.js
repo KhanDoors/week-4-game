cfd52527105dc6650f53ee92aa8db95204d1359a
unknown <syyadkhan@yahoo.com>
2018-03-04 20:24:01 -0500

    mar42023

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,12 +4,30 @@ var lost = 0;
 var win = 0;
 var previous = 0;
 
+
+
+
+
+
 var resetAndStart = function () {
+   
     $(".crystals").empty();
 
-    random_result = Math.floor(Math.random() * 69) + 30;
+    
+	var images = [
+        'http://cdn.playbuzz.com/cdn/7a5d7935-6177-4be8-8b72-2a95ad2bcdfe/3b295cc9-7b5e-412f-8b1f-8547edd8e66b.jpg', 
+        'http://vignette3.wikia.nocookie.net/marvel-contestofchampions/images/1/1c/2-Star_Crystal.png/revision/latest?cb=20150825213642', 
+        'http://jonvilma.com/images/crystal-5.jpg', 
+        'https://static.turbosquid.com/Preview/2014/07/08__10_08_09/Crystals0010.jpgc22b2831-ae7a-4cb6-b4ac-612aa7f35ad7Original.jpg'];
+    
+
+    random_result = Math.floor(Math.random() * 69 ) + 30;
+
+
     $("#result").html('Random Result: ' + random_result);
+  
     for(var i = 0; i < 4; i++){
+
          var random = Math.floor(Math.random() * 11) + 1;
        
         var crystal = $("<div>");
@@ -17,34 +35,59 @@ var resetAndStart = function () {
             "class": 'crystal',
             "data-random": random
         });
-                 
+          
+        crystal.css({
+            "background-image":"url('" + images[i] + "')",
+            "background-size":"cover"
+
+        });
+
      $(".crystals").append(crystal);
-     }
+     
+    }
+
      $("#previous").html("Total Score:" + previous);
-}
+
+    }
+
 
 resetAndStart();
 
+
+
 $(document).on('click', ".crystal", function () {
+
 var num = parseInt($(this).attr('data-random'));
+
 previous += num;
+
 
 $("#previous").html("Total Score:" + previous);
 
 console.log(previous);
+
 if(previous > random_result){
+    
     lost++;
+
     $("#lost").html("Sorry you Lost!" + lost);
+    
     previous = 0;
 
        resetAndStart();
+
 }
 else if(previous === random_result){
+
     win++;
-    $("#win").html("You Won!" + win);
+
+    $("#win").html("You Won: " + win);
 
     previous = 0;
+
     resetAndStart();
+
 }
+
 });
 
